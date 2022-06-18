0727bb72642b109aba92c3f0a26f49c822e930d3
Zulfikar Ali Muzakir <zulfikar26error@gmail.com>
2022-06-18 11:14:37 +0700

    add foreign key to notes owner column

---
+++ migrations/1655524475852_add-foreign-key-to-owner-column.js
@@ -0,0 +1,25 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(
+    "INSERT INTO users(id, username, password, fullname) VALUES ('old_notes', 'old_notes', 'old_notes', 'old_notes')"
+  );
+
+  pgm.sql("UPDATE notes SET owner = 'old_notes' WHERE owner = NULL");
+
+  pgm.addConstraint(
+    'notes',
+    'fk_notes.owner_users.id',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE'
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('notes', 'fk_notes.owner+_users.id');
+
+  pgm.sql("UPDATE notes SET owner = NULL WHERE owner = 'old_notes'");
+
+  pgm.sql("DELETE FROM users WHERE id = 'old_notes'");
+};
